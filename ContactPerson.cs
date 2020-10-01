7f0db7b4ac3a1809cdfbcc91b443d9ed5c52b50f
kretika arora <kretikaarora@gmail.com>
2020-10-01 14:58:17 +0530

    uc2

---
+++ ContactPerson.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class ContactPerson
+    {
+
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zip;
+        public Double phoneNo;
+        public string email;
+        public ContactPerson(string firstName, string lastName, string address, string city, string state, int zip, double phoneNo, string email)
+
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNo = phoneNo;
+            this.email = email;
+
+        }
+        public void MentionDetails()
+        {
+
+            Console.WriteLine("Enter all thecontact details");
+            firstName = Console.ReadLine();
+            lastName = Console.ReadLine();
+            address = Console.ReadLine();
+            city = Console.ReadLine();
+            state = Console.ReadLine();
+            zip = Convert.ToInt32(Console.ReadLine());
+            phoneNo = Convert.ToDouble(Console.ReadLine());
+            email = Console.ReadLine();
+
+        }
+
+
+
+
+
+
+
+    }
+}
